b7f90f48d17ae3adf39e48d080d3fbfd0a8656eb
Antonin Messinger <antonin.messinger@gmail.com>
2019-07-16 22:39:57 +0200

    add basic admin

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const markdownToHTML = require('./_filters/markdown');
 const { imageResize } = require('./_filters/image');
 
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('admin');
   eleventyConfig.addPassthroughCopy('images');
 
   eleventyConfig.addNunjucksFilter('markdownToHTML', markdownToHTML);
